008e1ede334bee8aaac7df425e3664e4931badff
hast06001 <hast06001@E-PW024NW1>
2023-10-06 11:08:12 +0200

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder>=16 && ålder<=19)
+                Console.WriteLine("Du får var med på festen");
+            else
+                Console.WriteLine("Du får inte vara med på festen.");
+
+            Console.ReadKey();
+        }
+    }
+}
